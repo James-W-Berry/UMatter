7c49faf40179c8e22c107cad0595c62dbf116c85
James-W-Berry <james.berry.iv@gmail.com>
2020-02-08 00:50:59 -0500

    add username and password sign in method

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
 import Main from "./components/Main";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as firebase from "firebase";
+import getEnvVars from "./environment";
+
+envVars = getEnvVars("dev");
+console.log(envVars);
+const firebaseConfig = {
+  apiKey: envVars.API_KEY,
+  authDomain: envVars.AUTH_DOMAIN,
+  databaseURL: envVars.DATABASE_URL,
+  projectId: envVars.PROJECT_ID,
+  storageBucket: envVars.STORAGE_BUCKET,
+  messagingSenderId: envVars.MESSAGING_SENDER_ID,
+  appId: envVars.APP_ID,
+  measurementId: envVars.MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
   constructor(props) {
--- components/Main.js
+++ components/Main.js
@@ -10,6 +10,9 @@ import NewJournalEntry from "./NewJournalEntry";
 import JournalEntry from "./JournalEntry";
 import MomentVisualization from "./MomentVisualization";
 import checkIfFirstLaunch from "./utils/checkIfFirstLaunch";
+import * as firebase from "firebase";
+import { Text, View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
 
 const MainNavigator = createStackNavigator({
   LandingPage: {
@@ -54,36 +57,77 @@ class Main extends Component {
     super(props);
     this.state = {
       isFirstLaunch: false,
-      hasCheckedAsyncStorage: false
+      hasCheckedAsyncStorage: false,
+      isLoggedIn: false,
+      isLoading: true
     };
   }
 
   async componentWillMount() {
     const isFirstLaunch = await checkIfFirstLaunch();
     this.setState({ isFirstLaunch, hasCheckedAsyncStorage: true });
+    let _this = this;
+
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        _this.setState({ isLoggedIn: true });
+        _this.setState({ isLoading: false });
+      } else {
+        _this.setState({ isLoggedIn: false });
+        _this.setState({ isLoading: false });
+      }
+    });
+  }
+
+  createNewUser(email, password) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(`${errorCode}: ${errorMessage}`);
+      });
   }
 
   render() {
-    console.log(this.state);
     const { hasCheckedAsyncStorage, isFirstLaunch } = this.state;
-    if (!hasCheckedAsyncStorage) {
-      return null;
-    }
 
-    return isFirstLaunch ? (
+    return this.state.isLoading ? (
+      <Text>Loading</Text>
+    ) : !hasCheckedAsyncStorage ? null : isFirstLaunch ? (
       <AppContainer
         ref={navigatorRef => {
           NavigationService.setTopLevelNavigator(navigatorRef);
           NavigationService.navigate("LandingPage");
         }}
       />
-    ) : (
+    ) : this.state.isLoggedIn ? (
       <AppContainer
         ref={navigatorRef => {
           NavigationService.setTopLevelNavigator(navigatorRef);
           NavigationService.navigate("Home");
         }}
       />
+    ) : (
+      <View>
+        <TextInput
+          label="Email"
+          onChangeText={text => this.setState({ email: text })}
+        ></TextInput>
+        <TextInput
+          label="Password"
+          onChangeText={text => this.setState({ password: text })}
+        ></TextInput>
+
+        <Button
+          onPress={() =>
+            this.createNewUser(this.state.email, this.state.password)
+          }
+        >
+          Create New Account
+        </Button>
+      </View>
     );
   }
 }
